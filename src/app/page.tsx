6062e3a90caf99a72945adfbd257ac2f00ce9251
Andrey Kotlyar <kotlyar562@gmail.com>
2026-04-05 12:45:01 +0300

    refactor: use Footer component on main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,10 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { ThemeSwitcher } from "@/components/theme-switcher";
-
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col font-sans">
       <main className="flex-1"></main>
-      <footer className="flex justify-end p-1">
-        <ThemeSwitcher />
-      </footer>
     </div>
   );
 }
