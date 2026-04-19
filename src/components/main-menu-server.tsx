8b8d96217ded1483b787b66e8900b250dc5579d6
Andrey Kotlyar <kotlyar562@gmail.com>
2026-04-19 11:32:24 +0300

    chore(components): use cache in main menu data loading

--- src/components/main-menu-server.tsx
+++ src/components/main-menu-server.tsx
@@ -2,11 +2,15 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import { cacheLife } from "next/cache";
+
 import { type CoursesList, coursesApi } from "@/features/courses";
 
 import { MainMenu } from "./main-menu";
 
 export async function MainMenuServer() {
+  "use cache";
+  cacheLife("hours");
   let courses: CoursesList | null = null;
   try {
     courses = (await coursesApi.fetchCourses()).results;
