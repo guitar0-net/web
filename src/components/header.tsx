d0aeb37bc4c85058ddeb7e3cf5f0c8d5d6a845ef
Andrey Kotlyar <kotlyar562@gmail.com>
2026-05-10 15:36:09 +0300

    refactor(ui): increase padding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ import { MainMenuServer } from "./main-menu";
 export function Header() {
   return (
     <header className="bg-background/80 sticky top-0 z-20 w-full border-b backdrop-blur-md">
-      <div className="container mx-auto grid grid-cols-3 items-center">
+      <div className="container mx-auto grid grid-cols-3 items-center p-1">
         <Link href={"/"}>
           <Logo />
         </Link>
