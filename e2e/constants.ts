c67a9cf4198ea730b90e99de975ac76128655abb
zhenya194 <zhenya.pro1753@gmail.com>
2026-05-13 14:28:22 +0300

    Return old .gitignore and Makefile

--- e2e/constants.ts
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2026 Andrey Kotlyar <guitar0.app@gmail.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://api.staging.guitar0.net";
-
-export const APP_PORT = 3001;
